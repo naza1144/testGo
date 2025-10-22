0bb1bf837495ff81782b73007811be464db70299
naza1144 <85209746+naza1144@users.noreply.github.com>
2025-10-22 15:11:22 +0700

    test-jslogin

--- static/js/login.js
+++ static/js/login.js
@@ -1,1 +1,19 @@
-console.log("This is the login template script.");
+async function  login(event) {
+    event.preventDefault();
+    const username = document.querySelector('username').value.trim();
+    const password = document.querySelector('password').value.trim();
+
+    const res = await fetch('/api/users/login', {
+        metod: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+    });
+
+    const data = await Response.json();
+
+    if (data.status == "ok") {
+        window.location.href = "/templates/page/succes";
+    } else {
+        alert("❌ อีเมลหรือรหัสผ่านไม่ถูกต้อง");
+    }
+}
